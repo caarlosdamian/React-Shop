23f477146a82f559946dee4213dbf1f836f8099a
caarlosdamian <dapecarlos@gmail.com>
2021-08-28 14:15:33 -0500

    Fix/Home desing refactor

--- src/components/HomeCard/home.css.js
+++ src/components/HomeCard/home.css.js
@@ -2,14 +2,29 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles({
   root: {
     marginTop: "20px",
-    borderRadius:"10px"
+    position: "relative",
+    border:"none",
   },
   media: {
     height: 0,
     paddingTop: "56.25%", // 16:9
+    borderRadius: "10px",
   },
   pos: {
     marginBottom: 12,
+  },
+  typo: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    padding: "10px",
+    backgroundColor: "rgba(143 , 143, 143, 0.5)",
+    borderRadius: "10px",
+    transition: "all 0.5s ease-out",
+    "&:hover": {
+      backgroundColor: "rgba(162, 162, 162, .8)",
+    },
   },
 });
 export default useStyles;
