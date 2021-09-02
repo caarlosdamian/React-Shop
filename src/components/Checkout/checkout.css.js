dc803449442f7b710fe9afe94053d4dd9e58aefc
caarlosdamian <dapecarlos@gmail.com>
2021-09-02 11:58:21 -0500

    Fix: change style to material ui

--- src/components/Checkout/checkout.css.js
+++ src/components/Checkout/checkout.css.js
@@ -1,7 +1,31 @@
 import { makeStyles } from "@material-ui/core";
 
 export const useStyles = makeStyles(() => ({
-    media: {
-        // paddingTop: "56.25%", // 16:9
-      },
-  }));
+  root: {
+    marginTop: "5rem",
+  },
+  table: {
+    minWidth: 650,
+  },
+  media: {
+    height: "6rem",
+    weight: "20rem",
+  },
+  total: {
+    marginLeft: "46rem",
+    fontSize: "30px",
+    textTransform: "uppercase",
+    fontWeight: "bold",
+  },
+  button: {
+    marginLeft: "45rem",
+    background: "black",
+    marginTop: "1em",
+    color: "white",
+    fontWeight: "bold",
+    width: "12rem",
+    padding: "10px",
+  },
+}));
+
+export default useStyles;
